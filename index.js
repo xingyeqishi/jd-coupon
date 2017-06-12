78a11755def233f51ece38821f4adf5017b83a72
sunxiaoshen <sunxiaoshen@threatbook.cn>
2017-06-12 19:12:46 +0800

    af

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var config = require('./config');
 var timer;
 
 config.forEach(function(item) {
-  getTicket(item, item.csUrl);
+  getTicket(item, item.url);
 });
 
 timer = setInterval(function() {
   config.forEach(function(item) {
     if (new Date().getTime() + 5000 > new Date(item.beginTime).getTime()) {
-      getTicket(item, item.csUrl);
+      getTicket(item, item.url);
     } else {
       console.log('time is not reached');
     }
